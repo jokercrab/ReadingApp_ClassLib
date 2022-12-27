95e1e2565c2a755bf89e33607762262979157f2d
jokercrab <anton20435@outlook.com>
2022-12-27 17:11:31 +0200

    Added project files

---
+++ Showcase/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HtmlAgilityPack;
+using System.IO;
+
+
+HtmlWeb web = new HtmlWeb();
+HtmlDocument doc = web.Load("https://www.wuxiaworld.eu/chapter/omniscient-readers-viewpoint-1");
+HtmlNodeCollection paragr = doc.DocumentNode.SelectNodes("//div[@id='chapterText']");
+
+using StreamWriter file = new("text");
+    
+foreach(var node in paragr)
+    file.WriteLine(node.InnerText);
+
+
+//Console.WriteLine(doc.DocumentNode.OuterHtml);
---
+++ TextScrapping/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace TextScrapping;
+using HtmlAgilityPack
+
+public class TextScrapper
+{
+	HtmlWeb web = new HtmlWeb();
+	HtmlDocument page;// = web.Load("https://www.wuxiaworld.eu/chapter/omniscient-readers-viewpoint-1");
+	HtmlNodeCollection main_text;// = doc.DocumentNode.SelectNodes("//div[@id='chapterText']");
+	HtmlNodeCollection next_link;// = doc.DocumentNode.SelectNodes("//div[@id='chapterText']");
+
+	public TextScrapper(string link)
+	{
+		page = web.Load(link);	
+		main_text = doc.DocumentNode.SelectNodes(Xpath);	
+	}
+
+	public scrapp_current_page(string Xpath)
+	{
+		main_text = doc.DocumentNode.SelectNodes(Xpath);
+	}
+}
