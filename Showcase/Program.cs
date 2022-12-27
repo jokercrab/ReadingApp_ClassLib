06cb2b1a888429f8e4d258e191942a0bc886ba9a
jokercrab <anton20435@outlook.com>
2022-12-27 19:01:18 +0200

    fixed main return method and test assembly

--- Showcase/Program.cs
+++ Showcase/Program.cs
@@ -1,16 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HtmlAgilityPack;
 using System.IO;
+using TextScrapping;
 
 
-HtmlWeb web = new HtmlWeb();
-HtmlDocument doc = web.Load("https://www.wuxiaworld.eu/chapter/omniscient-readers-viewpoint-1");
-HtmlNodeCollection paragr = doc.DocumentNode.SelectNodes("//div[@id='chapterText']");
+//HtmlWeb web = new HtmlWeb();
+//HtmlDocument doc = web.Load("https://www.wuxiaworld.eu/chapter/omniscient-readers-viewpoint-1");
+//HtmlNodeCollection paragr = doc.DocumentNode.SelectNodes("//div[@id='chapterText']");
 
 using StreamWriter file = new("text");
     
-foreach(var node in paragr)
-    file.WriteLine(node.InnerText);
+//foreach(var node in paragr)
+//   file.WriteLine(node.InnerText);
+TextScrapper scrapper = new TextScrapper("https://www.wuxiaworld.eu/chapter/omniscient-readers-viewpoint-1");
+file.WriteLine(scrapper.scrapp_current_page("//div[@id='chapterText']"));
 
-
-//Console.WriteLine(doc.DocumentNode.OuterHtml);
+Console.WriteLine("Done!");
--- TextScrapping/Class1.cs
+++ TextScrapping/Class1.cs
@@ -1,22 +1,29 @@
 ﻿namespace TextScrapping;
-using HtmlAgilityPack
+using HtmlAgilityPack;
+using System.Text;
 
 public class TextScrapper
 {
-	HtmlWeb web = new HtmlWeb();
+	HtmlWeb web;
 	HtmlDocument page;// = web.Load("https://www.wuxiaworld.eu/chapter/omniscient-readers-viewpoint-1");
-	HtmlNodeCollection main_text;// = doc.DocumentNode.SelectNodes("//div[@id='chapterText']");
-	HtmlNodeCollection next_link;// = doc.DocumentNode.SelectNodes("//div[@id='chapterText']");
+	//HtmlNodeCollection main_text;// = doc.DocumentNode.SelectNodes("//div[@id='chapterText']");
+	//HtmlNodeCollection next_link;// = doc.DocumentNode.SelectNodes("//div[@id='chapterText']");
 
 	public TextScrapper(string link)
 	{
-		page = web.Load(link);	
-		main_text = doc.DocumentNode.SelectNodes(Xpath);	
+		web = new HtmlWeb();
+		page = web.Load(link);
 	}
 
-	public string scrapp_current_page(string Xpath)
+	public StringBuilder scrapp_current_page(string Xpath)
 	{
-		main_text = doc.DocumentNode.SelectNodes(Xpath);
-		return main_text;
+		var Nodes = page.DocumentNode.SelectNodes(Xpath);
+		StringBuilder text = new StringBuilder();
+		foreach (var paragraph in Nodes)
+		{
+			text.Append(paragraph.InnerText);
+		}
+
+		return text;
 	}
 }
