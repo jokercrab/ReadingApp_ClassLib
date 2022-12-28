380aa9ec6e50d46abcb3f6ae3458becf647b3945
jokercrab <anton20435@outlook.com>
2022-12-28 21:54:01 +0200

    Fixed new line issue

--- TextScrapping/Class1.cs
+++ TextScrapping/Class1.cs
@@ -1,29 +1,28 @@
-﻿namespace TextScrapping;
+﻿//namespace TextScrapping;
 using HtmlAgilityPack;
 using System.Text;
 
 public class TextScrapper
 {
-	HtmlWeb web;
-	HtmlDocument page;// = web.Load("https://www.wuxiaworld.eu/chapter/omniscient-readers-viewpoint-1");
-	//HtmlNodeCollection main_text;// = doc.DocumentNode.SelectNodes("//div[@id='chapterText']");
-	//HtmlNodeCollection next_link;// = doc.DocumentNode.SelectNodes("//div[@id='chapterText']");
+    private HtmlWeb web;
+    private HtmlDocument page;
+    public TextScrapper(string link)
+    {
+        web = new HtmlWeb();
+        page = web.Load(link);
 
-	public TextScrapper(string link)
-	{
-		web = new HtmlWeb();
-		page = web.Load(link);
-	}
+    }
 
-	public StringBuilder scrapp_current_page(string Xpath)
-	{
-		var Nodes = page.DocumentNode.SelectNodes(Xpath);
-		StringBuilder text = new StringBuilder();
-		foreach (var paragraph in Nodes)
-		{
-			text.Append(paragraph.InnerText);
-		}
-
-		return text;
-	}
+    public StringBuilder ScrappCurrentPageParagraphs(string Xpath)
+    {
+        var Nodes = page.DocumentNode.SelectNodes(Xpath);
+        StringBuilder text = new StringBuilder();
+        foreach (var paragraph in Nodes)
+        {
+            text.Append(paragraph.InnerText);
+            text.Append('\n');
+        }
+        text.Remove(text.Length - 1, 1);
+        return text;
+    }
 }
