1db4bce31e45ac73e68a0fde9f0de3308e87e9e7
jokercrab <anton20435@outlook.com>
2023-01-08 22:59:59 +0200

    Added App Project

---
+++ App/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using System.Collections;
+
+namespace App
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+            SemanticScreenReader.Default.Announce(CounterBtn.Text + "times");
+        }
+        //private void onEntryComplete
+    }
+}
